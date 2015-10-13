ed00b8ed1cd8c1d5bbf130598c9565a85cbc3458
cschmitz <schmitz.chris@gmail.com>
2015-10-13 06:32:06 -0500

    Fixes brand tab creation/selection so that a brand with an existing tab isn't re-created.

--- public/app/view/inventory/details/InventoryController.js
+++ public/app/view/inventory/details/InventoryController.js
@@ -14,8 +14,13 @@ Ext.define('InventoryDemo.view.inventory.details.InventoryController', {
         var record = this.getViewModel().get('inventoryitem');
         record.getProxy().setExtraParam('_method', 'DELETE');
         record.erase();
-        this.getView().fireEvent('refreshList');
         this.getView().destroy();
+
+        // see if there's a better way of handling this. If the record is really linked to 
+        // the list's viewmodel you'd think the refresh would happen automatically as part
+        // of the `record.erase()`. There should be some better way of doing this where it
+        // affects the local store, not pull again from the server.
+        this.getView().fireEvent('refreshList');
     }
     
 });
--- public/app/view/main/MainController.js
+++ public/app/view/main/MainController.js
@@ -3,21 +3,35 @@ Ext.define('InventoryDemo.view.main.MainController', {
 
     alias: 'controller.main',
 
+    config: {
+        openTabs: new Array
+    },
+
     onOpenBrandTab:function (record, e){
+        debugger;
         var tabset = this.lookupReference('inventoryTabSet');
-        var newtab = Ext.create('InventoryDemo.view.inventory.list.Inventory',{
-            title: record.get('name')
+        var existingTab = this.getOpenTabs().filter(function (tab){
+            return (record.get('name') == tab.title);
         });
 
-        // note, this *shouldn't* be the way we have to make this request,
-        // but I'm not exactly sure how to manipulate the sencha proxy yet
-        // to rebuild the url to handle the proper rest url to make the
-        // request, so we're hacking it out.
-        var store = newtab.getViewModel().getStore('inventory');
-        store.getProxy().setExtraParam('brandid', record.getId());
-        store.load();
+        if(existingTab.length === 0){
+            var newtab = Ext.create('InventoryDemo.view.inventory.list.Inventory',{
+                title: record.get('name')
+            });
 
-        tabset.add(newtab);
-        tabset.setActiveTab(newtab);
+            // note, this *shouldn't* be the way we have to make this request,
+            // but I'm not exactly sure how to manipulate the sencha proxy yet
+            // to rebuild the url to handle the proper rest url to make the
+            // request, so we're hacking it out.
+            var store = newtab.getViewModel().getStore('inventory');
+            store.getProxy().setExtraParam('brandid', record.getId());
+            store.load();
+
+            tabset.add(newtab);
+            this.getOpenTabs().push(newtab);
+            tabset.setActiveTab(newtab);
+        } else {
+            tabset.setActiveTab(existingTab[0]);
+        }
     }
 });
